82096714f8eb7da6f976df7f5f633c6bb435a452
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2023-03-24 19:49:00 +0000

    dvc data ingestion

--- src/Spam/entity/config_entity.py
+++ src/Spam/entity/config_entity.py
@@ -7,7 +7,7 @@ class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name: str = training_pipeline.PIPELINE_NAME
-        self.artifact_dir: str = os.path.join(training_pipeline.ARTIFACT_DIR,timestamp)
+        self.artifact_dir: str = os.path.join(training_pipeline.ARTIFACT_DIR)
         self.timestamp: str = timestamp
 
 
