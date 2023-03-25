f0d11d49930a353bb3ab1fe9942d2cb7806b71cd
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2023-03-25 08:24:11 +0000

    common utils

--- src/Spam/utils/common.py
+++ src/Spam/utils/common.py
@@ -1,4 +1,4 @@
-import os
+import sys,os
 from box.exceptions import BoxValueError
 import yaml
 from Spam.logger import logging
@@ -8,6 +8,9 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+from Spam.exception import SpamException
+import numpy as np
+import dill
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
@@ -29,3 +32,42 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+
+
+
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """ To save numpy array data to a file
+    Args:
+        file_path (str): str location of file to save
+        array (np.array): np.array data to save
+    Raises:
+        FakeNewsException
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise SpamException(e, sys) from e
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise SpamException(e, sys) from e
+
+
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} does not exists")
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise SpamException(e, sys) from e
