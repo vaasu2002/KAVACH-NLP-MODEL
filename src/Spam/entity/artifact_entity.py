535b660ad6c1f7ed8542ebc0f62751e7e979a8a8
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2023-03-25 08:22:34 +0000

    artifact entity

--- src/Spam/entity/artifact_entity.py
+++ src/Spam/entity/artifact_entity.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(frozen=True)
 class DataIngestionArtifact:
     trained_file_path: str
     test_file_path: str
+
+@dataclass(frozen=True)
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
